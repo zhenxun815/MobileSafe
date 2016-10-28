c35b1378c192d7bb099175890f0874dc1dbf9d7a
zhenxun815 <guoyiheng89@gmail.com>
2016-10-28 13:04:28 +0800

    HomepageActivity

--- app/src/main/java/com/yiheng/mobilesafe/HomepageActivity.java
+++ app/src/main/java/com/yiheng/mobilesafe/HomepageActivity.java
@@ -1,17 +1,27 @@
 package com.yiheng.mobilesafe;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 
 public class HomepageActivity extends AppCompatActivity {
 
 
     private ImageView iv_homepage_heima;
     private GridView gv_homepage;
+    private static final String[] OPTION_TITLE = {
+            "手机防盗","骚扰拦截","软件管家","进程管理","流量统计","手机杀毒","缓存清理","常用工具"};
+    private static final int[] IMAGE_IDS={
+            R.drawable.sjfd,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +31,41 @@ public class HomepageActivity extends AppCompatActivity {
         iv_homepage_heima = (ImageView) findViewById(R.id.iv_homepage_heima);
         gv_homepage = (GridView) findViewById(R.id.gv_homepage);
 
+        ObjectAnimator animator = ObjectAnimator.ofFloat(iv_homepage_heima,"rotationY",0f,360f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setRepeatMode(ValueAnimator.RESTART);
+        animator.setDuration(2000);
+        animator.start();
+
+        gv_homepage.setAdapter(new MyAdaptor());
+
 
     }
 
     public void setting(View view) {
         // TODO: 2016/10/27 0027 设置按钮
 
+    }
+
+    private class MyAdaptor extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
     }
 }
